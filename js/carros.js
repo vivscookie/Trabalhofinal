d9ac170d30c81c963667f2377c1aba1f5d605cbd
Vivs <viih_almada@hotmail.com>
2025-12-22 15:33:20 +0000

    carros dados

---
+++ js/carros.js
@@ -0,0 +1,24 @@
+class Carro{
+    constructor(imagem,nome,portas,trans,bag,comb,preco,classe){
+        this.imagem = imagem;
+        this.nome = nome;
+        this.portas = portas;
+        this.trans = trans;
+        this.bag = bag;
+        this.comb = comb;
+        this.preco = preco;
+        this.classe = classe;
+    }
+}
+
+const sally = new Carro ("/imagens/sally.png","Sally",2,"Automático","Pequeno","Gasolina",70,"S");
+const mcqueen = new Carro ("/imagens/mcqueen.png","Relâmpago McQueen",2,"Manual","Não","Diesel",100,"S");
+const mate = new Carro ("/imagens/mate.png","Tom Mate",2,"Manual","Grande","Gasolina",23,"B");
+const mack = new Carro ("/imagens/mack.png","Mack",2,"Manual","Grande","Diesel",90,"A");
+const xerife = new Carro ("/imagens/xerife.png","Xerife",2,"Manual","Grande","Gasolina",30,"B");
+const fillmore = new Carro ("imagens/fillmore.png","Fillmore",3,"Manual","Grande","Gasolina",50,"B");
+const ramone = new Carro ("/imagens/ramone.png","Ramone",4,"Manual","Grande","Gasolina",60,"A");
+const sarge = new Carro ("/imagens/sarge.png","Sargento",2,"Manual","Médio","Gasolina",40,"B");
+const ruivo = new Carro ("/imagens/ruivo.png","Ruivo",2,"Manual","Grande","Diesel",90,"A");
+const guido = new Carro ("/imagens/guido.png","Guido",2,"Manual","Minúsculo","Gasolina",10,"B");
+const chick = new Carro ("/imagens/chick hicks.png","Chick Hicks",2,"Manual","Não","Diesel",95,"S");
